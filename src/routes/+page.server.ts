91f25560235ec384ce66b337330cef9d29d40dba
olivyou2 <olivyou2@gmail.com>
2025-11-04 21:43:18 +0900

    chore: Backend URL changed

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import type { TempLog } from "$lib/types/tempLog";
 
 export const load = async (params) => {
-    const fetchUrl = "http://localhost:8080/api/temp/?hours=144";
+    const fetchUrl = "https://backend.temp.suplitter.com/api/temp/?hours=144";
 
     const response = await fetch(fetchUrl);
     const data = await response.json();
